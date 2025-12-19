abb3e63d8e21ab533db141dfed85f69475d4e4a3
Anusorn <asomya45@gmail.com>
2025-12-19 11:21:45 +0700

    Start

---
+++ RedMacro/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("RedMacro")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("RedMacro")]
+[assembly: AssemblyTitle("RedMacro")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
